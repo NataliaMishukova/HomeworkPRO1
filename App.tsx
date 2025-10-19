533d5359f435a7e358e4fc83811eaa58ccf66036
NataliaMishukova <natali-7-1@mail.ru>
2025-10-19 18:41:00 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+
+import type { JSX } from 'react'
+import './App.css'
+import CarShop from './components/CarShop/CarShop'
+
+import Homework_02 from './components/Homework_02/Homework_02'
+import ProfileCard from './components/ProfileCard/ProfileCard'
+import Counter from './components/Counter/Counter'
+import Sandwich from './components/Sandwich/Sandwich'
+import Alcohol from './components/Alcohol/Alcohol'
+
+function App():JSX.Element {
+
+
+  return (
+   <div>
+     <Homework_02 />
+   { /*<Sandwich />*/}
+    { /*<Counter />*/}
+    
+   </div>
+  )
+}
+
+export default App
